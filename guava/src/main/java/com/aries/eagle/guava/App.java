62d2f4709130c23a99be4040e5497af297d6e82d
lihang17 <lihang17@meituan.com>
2019-10-25 15:45:56 +0800

    :heavy_plus_sign: import guava

--- guava/src/main/java/com/aries/eagle/guava/App.java
+++ guava/src/main/java/com/aries/eagle/guava/App.java
@@ -1,22 +1,34 @@
 package com.aries.eagle.guava;
 
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.RandomAccess;
+import java.util.Set;
 
 /**
  *
  */
 public class App {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(9);
-        list.add(5);
-        list.add(3);
-        list.add(7);
-        if(list.contains(1)){
-            list.remove(list.indexOf(1));
-        }
-        System.out.println(list);
+//        String bucket = "0,1,2,3,4,5,6,7,8,9";
+//        int size = Splitter.on(",").omitEmptyStrings().trimResults().splitToList(bucket).size();
+//        System.out.println(size/1000.0);
+        List list = new ArrayList();
+        list.add(1);
+        Set set = new HashSet<>(list);
+        System.out.println(set);
+    }
+}
+
+class A implements RandomAccess{
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
--- java-base/src/main/java/com/aries/eagle/java/base/App.java
+++ java-base/src/main/java/com/aries/eagle/java/base/App.java
@@ -1,15 +1,21 @@
 package com.aries.eagle.java.base;
 
 
-import com.google.common.base.Splitter;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class App {
-    public static void main(String[] args) {
-        String str = "101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200";
-        List<String> strings = Splitter.on(",").trimResults().splitToList(str);
-        System.out.println(strings.size()/10);
+    public static void main(String[] args) throws ParseException {
+        String key = "ab_dsp_T3";
+
+        System.out.println(key.substring(7));
     }
 }
+
+
 
