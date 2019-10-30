f858cefb52871d5614f7364b53292f276c447cee
lihang17 <lihang17@meituan.com>
2019-10-30 19:56:04 +0800

    :sparkles: netty demo

---
+++ java-base/src/main/java/com/aries/eagle/java/base/nettydemo/NettyClient.java
@@ -0,0 +1,36 @@
+package com.aries.eagle.java.base.nettydemo;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Date;
+
+
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        /*
+         * group：对应了我们IOClient.java中main函数起的线程
+         */
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+
+        while (true) {
+            channel.writeAndFlush(new Date() + ": hello world!");
+            Thread.sleep(2000);
+        }
+    }
+}
---
+++ java-base/src/main/java/com/aries/eagle/java/base/nettydemo/NettyServer.java
@@ -0,0 +1,35 @@
+package com.aries.eagle.java.base.nettydemo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+
+public class NettyServer {
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        NioEventLoopGroup boos = new NioEventLoopGroup();    // boos对应 IOServer.java中的接受新连接线程，主要负责创建新连接
+        NioEventLoopGroup worker = new NioEventLoopGroup();  // worker对应 IOClient.java中的负责读取数据的线程，主要用于读取数据以及业务逻辑处理
+
+        serverBootstrap
+                .group(boos, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                }).bind(8000);
+    }
+}
