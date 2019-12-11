2ef85959ce2edc8a8b5e98700ef0712448524e76
Kristin <kristinleejava@gmail.com>
2019-12-12 00:02:18 +0800

    :sparkles: nio demo

---
+++ netty-learn/src/main/java/com/kristin/nioExample/NioTest12.java
@@ -0,0 +1,13 @@
+package com.kristin.nioExample;
+
+/**
+ * @Author: Kristin
+ * @Date: 2019/12/11 23:50
+ * @Comment： Selector
+ * 服务器端监听5个端口号
+ */
+public class NioTest12 {
+    public static void main(String[] args) {
+
+    }
+}
